d41eea6b701ec1040f11a193cc7c1d6235f8055e
배상혁(학부생-소프트웨어전공) <baebea227@gmail.com>
2025-12-26 14:54:01 +0900

    Initial commit

---
+++ Floyd–Warshall.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    const int INF = 1e9;
+
+    vector<vector<int>> dist(n, vector<int>(n, INF));
+    for (int i=0; i<n; i++) dist[i][i] = 0;
+
+    for (int k=0; k<n; k++) {
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+}
