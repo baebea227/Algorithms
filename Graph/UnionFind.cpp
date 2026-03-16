15bf81b109fad0a7f2fc26167d8012e05070dd34
baebea227 <baebea227@gmail.com>
2026-03-16 15:58:52 +0900

    refactored UnionFind

--- Graph/UnionFind.cpp
+++ Graph/UnionFind.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int find(int x, vector<int>& parent) {
-    if(parent[x] == x)
-        parent[x] = find(parent[x], parent);
-    return parent[x];
+vector<int> parent;
+
+int find(int x) {
+    if(x == parent[x]) return x;
+    return parent[x] = find(parent[x]);
 }
 
-bool unite(int a, int b, vector<int>& parent) {
-    a = find(a, parent);
-    b = find(b, parent);
-    if(a == b) return false;
-    parent[b] = a;
+bool unite(int a, int b) {
+    int fa = find(a);
+    int fb = find(b);
+    if(fa == fb) return false;
+    parent[fb] = fa;
     return true;
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> parent(n);
-    for(int i=0; i<n; i++) {
-        parent[i] = i;
-    }
+
+    parent.resize(n);
+    for(int i=0; i<n; i++) parent[i] = i;
 }
